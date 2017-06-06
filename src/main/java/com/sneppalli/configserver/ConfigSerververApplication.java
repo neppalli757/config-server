263b0b9877975905ff03cd9c8fa4b340e88a27cf
skybot757 <sravan.neppalli@cgi.com>
2017-06-06 13:46:38 -0600

    second commit

--- src/main/java/com/sneppalli/configserver/ConfigSerververApplication.java
+++ src/main/java/com/sneppalli/configserver/ConfigSerververApplication.java
@@ -2,8 +2,10 @@ package com.sneppalli.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigSerververApplication {
 
 	public static void main(String[] args) {
