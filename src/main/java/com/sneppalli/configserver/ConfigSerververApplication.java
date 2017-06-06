6fe51cb401cb6838b5d009453974a3ee0b346534
skybot757 <sravan.neppalli@cgi.com>
2017-06-06 13:13:58 -0600

    first commit

---
+++ src/main/java/com/sneppalli/configserver/ConfigSerververApplication.java
@@ -0,0 +1,12 @@
+package com.sneppalli.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSerververApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSerververApplication.class, args);
+	}
+}
